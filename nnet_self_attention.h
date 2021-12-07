cf93c92ec4b9f50d0c2fc4453ed8a5ceebac3296
Fiwo735 <fiwo735@gmail.com>
2021-12-07 05:30:04 +0100

    Added Python tool for extracting weights and biases from a Pytorch model

--- nnet_self_attention.h
+++ nnet_self_attention.h
@@ -36,56 +36,57 @@ struct self_attention_config
 
 template<class data_T, class res_T, typename CONFIG_T>
 void self_attention(
-    data_T    data[CONFIG_T::n_in],
-    res_T     res[CONFIG_T::n_out],
-    typename CONFIG_T::weight_t  weights[CONFIG_T::n_in*CONFIG_T::n_out],
-    typename CONFIG_T::bias_t    biases[CONFIG_T::n_out])
+    data_T    data[CONFIG_T::d_k], // d_k == h
+    res_T     res[CONFIG_T::d_v], // d_v == h
+    typename CONFIG_T::weight_t  weights_Q[CONFIG_T::d_k*CONFIG_T::d_h], // d_k x h == h x h
+    typename CONFIG_T::weight_t  weights_K[CONFIG_T::d_k*CONFIG_T::d_h], // d_k x h == h x h
+    typename CONFIG_T::weight_t  weights_V[CONFIG_T::d_v*CONFIG_T::d_h]) // d_v x h == h x h
 {
 
-    // 1   x
-
-
     // TODO: implement separate weights and biases for Q/K/v
+    data_T zero_biases_k[CONFIG_T::d_k]; // TODO: do we have to initialize to 0 for an FPGA?
+    data_T zero_biases_v[CONFIG_T::d_v]; // TODO: do we have to initialize to 0 for an FPGA?
+    data_T zero_biases_kk[CONFIG_T::d_k*CONFIG_T::d_k]; // TODO: do we have to initialize to 0 for an FPGA?
 
     // 2.1 Q - queries, d_k entries
     // matrix: n_q x d_k
-    data_T Q_out[CONFIG_T::n_in];
+    data_T Q_out[CONFIG_T::d_k];
     // #pragma HLS ARRAY_PARTITION variable=Q_out complete dim=0
-    dense<data_T, data_T, CONFIG_T>(data, Q_out, weights, biases);
+    dense<data_T, data_T, CONFIG_T>(data, Q_out, weights_Q, zero_biases_k);
 
     // 2.2 K - keys, d_k entries
     // matrix: n_k x d_k
-    data_T K_out[CONFIG_T::n_in];
+    data_T K_out[CONFIG_T::d_k];
     // #pragma HLS ARRAY_PARTITION variable=K_out complete dim=0
-    dense<data_T, data_T, CONFIG_T>(data, K_out, weights, biases);
+    dense<data_T, data_T, CONFIG_T>(data, K_out, weights_K, zero_biases_k);
 
     // 2.3 V - values, d_v entries
     // matrix: n_k x d_v
-    data_T V_out[CONFIG_T::n_in];
+    data_T V_out[CONFIG_T::d_v];
     // #pragma HLS ARRAY_PARTITION variable=V_out complete dim=0
-    dense<data_T, data_T, CONFIG_T>(data, V_out, weights, biases);
+    dense<data_T, data_T, CONFIG_T>(data, V_out, weights_V, zero_biases_v);
 
     // 3   MatMul
     // Q * K^T
-    data_T QK_out[CONFIG_T::n_out];
+    // data_T QK_out[CONFIG_T::d_k*CONFIG_T::d_k];
+    data_T QK_out;
     // #pragma HLS ARRAY_PARTITION variable=QK_out complete dim=0
     // TODO: how to transpose K?
-    data_T zero_biases[CONFIG_T::n_out]; // TODO: do we have to initialize to 0 for an FPGA?
-    dense<data_T, data_T, CONFIG_T>(Q_out, QK_out, K_out, zero_biases);
+    dense<data_T, data_T, CONFIG_T>(Q_out, QK_out, K_out, zero_biases_kk);
 
     // 4   Scale (element-wise)
     // ... * sqrt(d_k)
-    data_T scaling_factor = 1 / 4; // TODO: implement actual sqrt of d_k
-    Scale: for (int iscale = 0; iscale < CONFIG_T::n_out; iscale++) {
-        if (CONFIG_T::io_type == io_serial){
-            #pragma HLS PIPELINE
-        }
-        QK_out *= scaling_factor;
-    }
+    // Scale: for (int iscale = 0; iscale < CONFIG_T::d_k*CONFIG_T::d_k; iscale++) {
+    //     if (CONFIG_T::io_type == io_serial){
+    //         #pragma HLS PIPELINE
+    //     }
+    //     QK_out *= CONFIG_T::inv_sqrt_d_k;
+    // }
+    QK_out *= CONFIG_T::inv_sqrt_d_k;
 
     // 5   Softmax
     // softmax(...)
-    data_T softmax_out[CONFIG_T::n_out];
+    data_T softmax_out[CONFIG_T::d_k*CONFIG_T::d_k];
     softmax<data_T, data_T, CONFIG_T>(QK_out, softmax_out);
 
     // 6   MatMul
