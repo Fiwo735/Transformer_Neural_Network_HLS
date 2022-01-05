0004840d620bc494b759205845dde26c0fb8fe85
Fiwo735 <fiwo735@gmail.com>
2022-01-05 04:12:45 +0100

    Working on the calculation embedding for norm layers

--- extract_weights_biases.py
+++ extract_weights_biases.py
@@ -1,16 +1,56 @@
 import torch
 import numpy as np
+from typing import Optional, List
 
-def extract_weights_biases(model_path: str, result_path:str, var_type:str = "model_default_t", flatten_order:str = "C"):
+def transform_norm_weights_biases(
+  arr: np.array,
+  is_weight: bool,
+  corr_old_weights: Optional[List[float]] = None,
+  eps: int = 1e-5
+) -> np.array:
+
+  # mean = arr.mean()
+  # var = arr.var()
+  denom = np.sqrt(var + eps)
+
+  # f_norm = None
+  normalized_arr = None
+  if is_weight:
+    # f_norm = lambda x: (x / denom)
+    normalized_arr = np.array([w / denom for w in arr])
+  else:
+    # f_norm = lambda x: (x - (corr_old_weights * mean / denom))
+    normalized_arr = np.array([b - corr_old_weights[i] * mean / denom for i, b in enumerate(arr)])
+
+  # normalized_arr = np.array(list(map(f_norm, arr)))
+  assert normalized_arr.shape == arr.shape,\
+    f'Array shape cannot change (old {arr.shape}, new {normalized_arr.shape}'
+
+  return normalized_arr
+
+
+def extract_weights_biases(
+  model_path: str,
+  result_path: str,
+  norm_layer_base_names: List[str],
+  var_type: str = "model_default_t",
+  flatten_order: str = "C",
+) -> None:
+
   model = torch.load(model_path)
   overview = []
   parameters_include = []
   load_weights_from_txt = []
 
-  # i = 0
+  previous_base = None
+  previous_was_weight = False
+  previous_weights = None
+
+  i = 0
 
   for layer_name, values in model['state_dict'].items():
-    # i += 1
+    i += 1
+    # print(model['state_dict'].keys())
 
     h_file_name = layer_name.replace('.', '_')
     parameters_include.append('#include "weights/' + h_file_name + '.h"')
@@ -30,6 +70,7 @@ def extract_weights_biases(model_path: str, result_path:str, var_type:str = "mod
     out_file.write("#ifndef " + header_guard_name + "\n")
     out_file.write("#define " + header_guard_name + "\n\n")
 
+    # transform row-major to column-major to match HLS data layout
     # print(layer_name)
     # print(values.shape)
     if len(values.shape) > 1:
@@ -46,6 +87,29 @@ def extract_weights_biases(model_path: str, result_path:str, var_type:str = "mod
       # print('flaten C final')
       # print(values)
 
+    # apply the mean/variance embedding for normalization layers
+    current_base = '.'.join(layer_name.split('.')[:-1])
+    # print(f'Checking if {layer_name} (with base {current_base}) is in norm_layer_base_names')
+    if current_base in norm_layer_base_names:
+      print(f'Applying mean/var calculation embedding to {layer_name}')
+      current_is_weight = layer_name.split('.')[-1] == 'weight'
+
+      if not current_is_weight:
+        assert previous_base == current_base and previous_was_weight,\
+          f'Layer base name {current_base} suggests it is a bias, but no corresponding weight was found'
+        assert len(values) == len(previous_weights),\
+          f'Layer base name {current_base} suggests it is a bias, but previous weights have different length'
+        
+        values = transform_norm_weights_biases(values, is_weight=current_is_weight, corr_old_weights=previous_weights)
+      
+      else:
+        previous_weights = values.copy()
+        values = transform_norm_weights_biases(values, is_weight=current_is_weight)
+
+      previous_base = current_base
+      previous_was_weight = current_is_weight
+
+    # print(values)
     elements_count = len(values)
     declaration = var_type + " " + h_file_name + "[" + str(elements_count) + "]"
     load_weights_from_txt.append(f'nnet::load_weights_from_txt<{var_type}, {elements_count}>({h_file_name}, "{h_file_name}.txt");')
@@ -92,6 +156,17 @@ def extract_weights_biases(model_path: str, result_path:str, var_type:str = "mod
 if __name__ == "__main__":
   model_path = "fyp21yuan_code/experiments/constituent_base/best.pth.tar"
   result_path = "extracted_weights_biases/"
-  #TODO which flatten_order is needed?
-  extract_weights_biases(model_path=model_path, result_path=result_path, flatten_order='C')
+  norm_layer_base_names = [
+    'out_layer.0',
+    'transformers.0.linear.0',
+    'transformers.0.linear.3',
+    'transformers.0.self_attention.norm',
+    'transformers.1.linear.0',
+    'transformers.1.linear.3',
+    'transformers.1.self_attention.norm',
+    'transformers.2.linear.0',
+    'transformers.2.linear.3',
+    'transformers.2.self_attention.norm',
+  ]
+  extract_weights_biases(model_path=model_path, result_path=result_path, norm_layer_base_names=norm_layer_base_names)
 
