82ea12c18fe38603dae3239b020ede0b48623d33
faw18 <faw18@ic.ac.uk>
2022-05-03 06:31:51 +0100

    Added 3x16 dataset option for Pytorch

--- scripts/get_data_30.sh
+++ scripts/get_data_30.sh
@@ -1,11 +1,11 @@
-cd fyp21yuan_code/
+data_dir="pytorch/data/30"
 
 wget https://zenodo.org/record/3601436/files/hls4ml_LHCjet_30p_train.tar.gz
-mkdir -p data/30 && cat *.tar.gz | tar -xzvf - --ignore-zeros --directory data/30/
+mkdir -p ${data_dir}
+cat *.tar.gz | tar -xzvf - --ignore-zeros --directory ${data_dir}
 rm -rf hls4ml_LHCjet_30p_train.tar.gz
 
 wget https://zenodo.org/record/3601436/files/hls4ml_LHCjet_30p_val.tar.gz
-mkdir -p data/30 && cat *.tar.gz | tar -xzvf - --ignore-zeros --directory data/30/
+mkdir -p ${data_dir}
+cat *.tar.gz | tar -xzvf - --ignore-zeros --directory ${data_dir}
 rm -rf hls4ml_LHCjet_30p_val.tar.gz
-
-python3 nn/build_dataset.py
